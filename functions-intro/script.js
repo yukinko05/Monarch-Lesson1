c361c6b3d1c625b4e5b526dddfc26f9a6b10c3cb
yukinko05 <133909119+yukinko05@users.noreply.github.com>
2023-06-10 13:51:24 +0900

    initial-commit
    Monarch-Lesson1の記録

---
+++ functions-intro/script.js
@@ -0,0 +1,49 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+//const school = "Code Chrysalis";
+//console.log(school); // "Code Chrysalis" を表示
+//let week = 1;
+//    console.log(week); // "1" を表示
+
+// ここにあなたのコードを書いてください
+//const name = '有希菜';
+//let age = '28';
+//let isProgrammer = 'true';
+//let currentTask = 1;
+//currentTask = currentTask + 1;
+//    console.log(name); //名前を表示
+//    console.log(age); // 年齢を表示
+//    console.log(isProgrammer); // "true" を表示
+//    console.log(currentTask); // "1" を表示
+
+// 正方形の面積を計算して保存
+// const squareSideLength = 2;
+// ここにあなたのコードを書いてください
+// let squareArea = squareSideLength * 2;
+//    console.log(squareArea);//"4"を表示
+
+// 長方形の面積を計算して保存
+// const rgthectangleBaseLen = 3;
+// const rectangleHeightLength = 4;
+// ここにあなたのコードを書いてください
+// let rectangleArea = rgthectangleBaseLen * rectangleHeightLength;
+//    console.log(rectangleArea); // "12" を表示
+
+// 三角形の面積を計算して保存
+// const triangleBaseLength = 4;
+// const triangleHeightLength = 5;
+// ここにあなたのコードを書いてください
+// let triangleArea = (triangleBaseLength * triangleHeightLength) / 2;
+//    console.log(triangleArea); // "10" を表示
+
+// 円の円周と面積を計算して保存する
+// const circleDiameter = 10;
+// const Pi = 3.141592;
+// ここにあなたのコードを書いてください
+// let circleCircumference = circleDiameter * Pi;
+// let circleArea = ((circleDiameter / 2) ** 2) * Pi;
+//    console.log(circleCircumference); // "31.41592653589793" に近い値を表示
+//    console.log(circleArea); // "78.53981633974483" に近い値を表示
+
+// 課題「値を直接再割り当てせずに a と b の値を入れ替えて、期待通りの値が出力されるようにしましょう。」以降のは後日対応
